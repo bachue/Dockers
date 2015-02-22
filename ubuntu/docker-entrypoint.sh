4d02a003fda05951ffbe7e944eb890e6aaa0f569
Bachue Zhou <bachue.shu@gmail.com>
2015-02-22 23:06:32 +0800

    should not stop docker if any error happens in the script

--- ubuntu/docker-entrypoint.sh
+++ ubuntu/docker-entrypoint.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
-set -e
-
 if [ ! -d "/root/.ssh" ]; then
     ssh-keygen -t rsa -N "" -f /root/.ssh/id_rsa
 fi
 
 if [ -d "/root/.gitconf" ]; then
-    cd /root/.gitconf
+    cd /root/.gitconf && \
     git pull --rebase origin master
 else
-    git clone git://github.com/bachue/My-Git-Config.git /root/.gitconf
-    ln -s /root/.gitconf/.gitconfig /root/.gitconfig
+    git clone git://github.com/bachue/My-Git-Config.git /root/.gitconf && \
+    ln -s /root/.gitconf/.gitconfig /root/.gitconfig && \
     ln -s /root/.gitconf/.gitignore_global /root/.gitignore_global
 fi
 
 if [ -d "/root/.gdbconf" ]; then
-    cd /root/.gdbconf
+    cd /root/.gdbconf && \
     git pull --rebase origin master
 else
-    git clone git://github.com/bachue/GDB-Config.git /root/.gdbconf
+    git clone git://github.com/bachue/GDB-Config.git /root/.gdbconf && \
     ln -s /root/.gdbconf/gdbinit /root/.gdbinit
 fi
 
