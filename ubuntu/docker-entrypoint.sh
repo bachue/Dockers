c6213d989527876ea350f751ea18b4f8dda03b6a
Bachue Zhou <bachue.shu@gmail.com>
2015-04-21 20:28:06 +0800

    remove gdb initialization script

--- ubuntu/docker-entrypoint.sh
+++ ubuntu/docker-entrypoint.sh
@@ -4,23 +4,6 @@ if [ ! -d "/root/.ssh" ]; then
     ssh-keygen -t rsa -N "" -f /root/.ssh/id_rsa
 fi
 
-if [ -d "/root/.gitconf" ]; then
-    cd /root/.gitconf && \
-    git pull --rebase origin master
-else
-    git clone git://github.com/bachue/My-Git-Config.git /root/.gitconf && \
-    ln -s /root/.gitconf/.gitconfig /root/.gitconfig && \
-    ln -s /root/.gitconf/.gitignore_global /root/.gitignore_global
-fi
-
-if [ -d "/root/.gdbconf" ]; then
-    cd /root/.gdbconf && \
-    git pull --rebase origin master
-else
-    git clone git://github.com/bachue/GDB-Config.git /root/.gdbconf && \
-    ln -s /root/.gdbconf/gdbinit /root/.gdbinit
-fi
-
 cd /root
 
 exec "$@"
